90e008dc6f419da35f4131130d7cbca26e5e7fb9
loubarb <louis.barbati@gmail.com>
2022-03-01 00:40:14 -0500

    Added styling

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ const placeholder = function (word) {
     const placeholderLetters = [];
     for (const letter of word) {
         // console.log(letter); // separates each letter in word into an array
-        placeholderLetters.push("●");
+        placeholderLetters.push("🟆");
     }
     wordInProgress.innerText = placeholderLetters.join("");
 };
@@ -109,7 +109,7 @@ const wordUpdateProgress = function (guessedLetters) {
         if (guessedLetters.includes(letter)) {
             revealWord.push(letter.toUpperCase());
         } else {
-            revealWord.push("●");
+            revealWord.push("🟆");
         }
     }
     wordInProgress.innerText = revealWord.join("");
